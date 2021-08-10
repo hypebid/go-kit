865bf9378b94b525e46f1fe3766d23f9e19efa5f
molson82 <wvandebelt82@yahoo.com>
2021-08-10 17:34:50 -0400

    :tada: created server and unary req id interceptor

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/hypebid/go-kit
 
 go 1.16
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
+	github.com/sirupsen/logrus v1.8.1
+	google.golang.org/grpc v1.39.1
+)
--- grpc/middleware/transactionId/serverInterceptors.go
+++ grpc/middleware/transactionId/serverInterceptors.go
@@ -1,7 +1,39 @@
 package middleware
 
-import "log"
+import (
+	"context"
 
-func Test() {
-	log.Println("test test from the go-kit")
+	"github.com/google/uuid"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+)
+
+type TransactionIdMarker string
+
+func UnaryServerInterceptor(logger *logrus.Logger) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		var newCtx context.Context
+		id := uuid.NewString()
+
+		logger.Printf("TransactionId created: %v", id)
+		newCtx = context.WithValue(ctx, TransactionIdMarker("transaction_id_ctx_marker"), id)
+
+		return handler(newCtx, req)
+	}
+}
+
+func StreamServerInterceptor(logger *logrus.Logger) grpc.StreamServerInterceptor {
+	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		var newCtx context.Context
+		id := uuid.NewString()
+
+		logger.Println("TransactionId created: %v", id)
+		newCtx = context.WithValue(stream.Context(), TransactionIdMarker("transaction_id_ctx_marker"), id)
+
+		wrapped := grpc_middleware.WrapServerStream(stream)
+		wrapped.WrappedContext = newCtx
+
+		return handler(srv, wrapped)
+	}
 }
